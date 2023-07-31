4a508f25485560e7bd474b99e3a709295cd42a10
코코형아 <92447290+Nbowow@users.noreply.github.com>
2023-07-31 21:53:31 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+fibo = [0 for _ in range(23)]
+
+fibo[1] = 1
+for i in range(2, n+1):
+    fibo[i] = fibo[i-1] + fibo[i-2]
+
+print(fibo[n])
