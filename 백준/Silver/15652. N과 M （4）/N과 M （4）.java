62a61c6961d17fea7630c1058962b03f7d5a43c0
코코형아 <92447290+Nbowow@users.noreply.github.com>
2024-01-25 23:49:29 +0900

    [Silver III] Title: N과 M (4), Time: 232 ms, Memory: 21240 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+/*
+        작성자 : 남보우
+        문제 : [S3] N과 M(2) - 15650
+        제출 : 2024년 1월 25일
+        결과 : 통과
+        성능 요약 : 메모리 15776KB, 시간 144ms
+        아이디어 : N과M1에서 오름차순 수열만 뽑은 것으로 백트래킹 기법으로 숫자를 넘겨줄 때
+                현재 index번호를 넘겨주어 그 index이후의 숫자만 체킹하도록 하였습니다.
+*/
+
+public class Main {
+
+    static int N;
+    static int M;
+    static List<Integer> ans = new ArrayList<>();
+    static StringBuilder sb = new StringBuilder();
+
+    static void backTracking(int count, int r) {
+        if (count == M) {
+            for (int num : ans) {
+                sb.append(num+1 + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = r; i < N; i++) {
+            ans.add(i);
+            backTracking(count + 1, i);
+            ans.remove(ans.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        int count = 0;
+        backTracking(count, 0);
+        System.out.println(sb);
+    }
+}
