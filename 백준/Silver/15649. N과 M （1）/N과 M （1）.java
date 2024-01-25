b3acb9e11756a844aba3f6417623e91480c86543
코코형아 <92447290+Nbowow@users.noreply.github.com>
2024-01-25 22:48:52 +0900

    [Silver III] Title: N과 M (1), Time: 380 ms, Memory: 36476 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N;
+    static int M;
+    static List<Integer> ans = new ArrayList<>();
+    static int[] visited;
+    static StringBuilder sb = new StringBuilder();
+
+    static void backTracking(int count) {
+        if (count == M) {
+            for (int num : ans) {
+                sb.append(num+1 + " ");
+            }
+            sb.append("\n");
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (visited[i] == 0) {
+                visited[i] = 1;
+                ans.add(i);
+                backTracking(count + 1);
+                visited[i] = 0;
+                ans.remove(ans.size() - 1);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new int[N];
+
+        int count = 0;
+        backTracking(count);
+        System.out.println(sb);
+    }
+}
