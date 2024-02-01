7b836012032cf952fc3085d885ee8853c31aaa49
코코형아 <92447290+Nbowow@users.noreply.github.com>
2024-02-01 17:08:44 +0900

    [Silver II] Title: DNA 비밀번호, Time: 312 ms, Memory: 21168 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -6,16 +6,13 @@ import java.util.StringTokenizer;
 public class Main {
 
 	static int S, P, ans;
-	static Character c;
 	static String dna;
 	static int[] acgt = new int[4];
-	static int[] isSelected = new int[4];
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		S = Integer.parseInt(st.nextToken());
-		isSelected = new int[S];
 		P = Integer.parseInt(st.nextToken());
 		
 		dna = br.readLine();
@@ -25,21 +22,20 @@ public class Main {
 			acgt[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		int[] temp = Arrays.copyOf(acgt, 4);
 		for (int i=0; i<P; i++) {
 			if (dna.charAt(i) == 'A') {
-				temp[0]--;
+				acgt[0]--;
 			} else if (dna.charAt(i) == 'C'){
-				temp[1]--;
+				acgt[1]--;
 			} else if (dna.charAt(i) == 'G') {
-				temp[2]--;
+				acgt[2]--;
 			} else {
-				temp[3]--;
+				acgt[3]--;
 			}
 		}
 		int flag = 0;
 		for (int i=0; i<4; i++) {
-			if (temp[i] > 0) {
+			if (acgt[i] > 0) {
 				flag = 1;
 				break;
 			}
@@ -48,29 +44,29 @@ public class Main {
 		
 		for (int i=P; i<S; i++) {
 			if (dna.charAt(i) == 'A') {
-				temp[0]--;
+				acgt[0]--;
 			} else if (dna.charAt(i) == 'C'){
-				temp[1]--;
+				acgt[1]--;
 			} else if (dna.charAt(i) == 'G') {
-				temp[2]--;
+				acgt[2]--;
 			} else {
-				temp[3]--;
+				acgt[3]--;
 			}
 			
 			int numTemp = i-P;
 			if (dna.charAt(numTemp) == 'A') {
-				temp[0]++;
+				acgt[0]++;
 			} else if (dna.charAt(numTemp) == 'C'){
-				temp[1]++;
+				acgt[1]++;
 			} else if (dna.charAt(numTemp) == 'G') {
-				temp[2]++;
+				acgt[2]++;
 			} else {
-				temp[3]++;
+				acgt[3]++;
 			}
 			
 			flag = 0;
 			for (int j=0; j<4; j++) {
-				if (temp[j] > 0) {
+				if (acgt[j] > 0) {
 					flag = 1; 
 					break;
 				}
