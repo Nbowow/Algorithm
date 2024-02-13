07ec229a6de3b25cdef2359f2a9617328a991914
코코형아 <92447290+Nbowow@users.noreply.github.com>
2024-02-13 17:09:06 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 4,292 ms, Memory: 21,984 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -1,24 +1,22 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
 /*
 	@author : 남보우
-	문제 : [D3] 햄버거 다이어트 -5215번
-	제출 : 2024년 2월 8일
+	문제 : [D3] 규영이와 인영이의 카드게임 - 6808번
+	제출 : 2024년 2월 13일
 	결과 : 통과
-	소요 시간 : 10분
-	아이디어 : 과일들을 정렬한 후 
+	소요 시간 : 60분
+	아이디어 : 규영이와 인영이의 카드를 입력 받은 후, 순열로 모든 경우의수를 다 계산하였습니다. 
 */
 public class Solution {
 	
 	static int win, lose;
 	static List<Integer> gyuCard;
 	static List<Integer> inCard;
-	static int totalScore;
 	static boolean[] isVisited;
 	static int[] perm;
 	
@@ -28,18 +26,22 @@ public class Solution {
 			int gyuPoints = 0;
 			int inPoints = 0;
 			for (int i=0; i<9; i++) {
-				// 규영이가 더 수가 클경우
+				// 규영이의 수가 더 클 경우
 				if (gyuCard.get(i) > inCard.get(perm[i])) {
 					gyuPoints += gyuCard.get(i) + inCard.get(perm[i]);
 				}
+				// 인영이의 수가 더 클 경우
 				else inPoints += gyuCard.get(i) + inCard.get(perm[i]);
 			}
 			
+			// 규영이의 포인트가 더 크면 규영이 승
 			if (gyuPoints > inPoints) win++;
+			// 그렇지 않으면 인영이 승
 			else lose++;
 		}
 		
 			
+		// 순열로 모든 경우의수를 탐색
 		for (int i=0; i<9; i++) {
 			if(!isVisited[i]) {
 				isVisited[i] = true;
@@ -49,7 +51,6 @@ public class Solution {
 			}
 		}
 			
-		
 	}
 	
 	public static void main(String[] args) throws Exception {
@@ -61,43 +62,26 @@ public class Solution {
 			st = new StringTokenizer(br.readLine());
 			win = 0;
 			lose = 0;
-			totalScore = 0;
 			isVisited = new boolean[9];
 			perm = new int[9];
 			gyuCard = new ArrayList<>();
 			inCard = new ArrayList<>();
+			// 입력받은 수 = 규영이의 카드
 			for (int i=0; i<9; i++) {
 				gyuCard.add(Integer.parseInt(st.nextToken()));
 			}
 			
+			// 1~18까지의 수 중 입력받지 않은 수 인영이의 카드로 저장
 			for (int i=1; i<=18; i++) {
 				if (!gyuCard.contains(i)) {
 					inCard.add(i);
 				}
 			}
 			
-//			printArray();
-			
-			
 			dfs(0);
 			
 			System.out.println("#" + tc + " " + win + " " + lose);
 			
-			
 		}
 	}
-	
-	static void printArray() {
-		for (int i=0; i<9; i++) {
-			System.out.print(gyuCard.get(i) + " ");
-		}
-		System.out.println();
-		
-		for (int i=0; i<9; i++) {
-			System.out.print(inCard.get(i) + " ");
-		}
-	}
-
-	
-
 }
