5c5322209d4bb131bfd3c4701b072fc085e7b291
코코형아 <92447290+Nbowow@users.noreply.github.com>
2024-02-16 11:57:10 +0900

    [Gold V] Title: Moo 게임, Time: 120 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Gold V/5904. Moo 게임/Moo 게임.java
+++ 백준/Gold V/5904. Moo 게임/Moo 게임.java
@@ -1,6 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
@@ -9,47 +8,25 @@ public class Main {
 
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
 		S = new long[30];
 		S[0] = 3;
-		
-		for (int i=1; i<30; i++) {
+		for (int i=1; i<S.length-1; i++) {
 			S[i] = S[i-1] * 2 + (i+3);
 		}
+        
+		int N = Integer.parseInt(br.readLine());
 		
-//		System.out.println(Arrays.toString(S));
-
-		int startIndex = 0;
-		for (int i=0; i<S.length-1; i++) {
-			if (N<S[i]) {
-				startIndex = i;
-				break;
-			}
-		}
-		
-		boolean isChecked = false;
-		for (int i=startIndex-1; i>=0; i--) {
-			if (N > S[i]) {
-				N -= S[i];
-				if (N < i+4) {
-					if (N == 1) ans = 'm';
-					else ans = 'o';
-					
-					isChecked = true;
-					break;
+xx:		for (int startIndex=0; startIndex<S.length-1; startIndex++) {
+			if (N<S[startIndex]) {
+				for (int i=startIndex-1; i>=0; i--) {
+					if (N > S[i]) {
+						N -= S[i];
+						if (N < i+4) break xx;
+						N -= (i+4);
+					}
 				}
-				N -= (i+4);
 			}
 		}
-		
-		if (!isChecked) {
-			if (N == 1) ans = 'm';
-			else ans = 'o';
-		}
-
-		System.out.println(ans);
-		
+		System.out.println(N == 1 ? 'm' : 'o');
 	}
-
 }
