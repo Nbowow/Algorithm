13b6a3ba6ffd6db781d31875959088dac1f53db1
코코형아 <92447290+Nbowow@users.noreply.github.com>
2024-02-20 12:00:12 +0900

    [Silver II] Title: 섬의 개수, Time: 164 ms, Memory: 16004 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.java
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -4,8 +4,20 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/*
+	@author : 남보우
+	문제 : [S2] 섬의 개수 - 4963번
+	제출 : 2024년 2월 20일
+	결과 : 통과
+	성능 요약 : 메모리 16344KB, 시간 180ms
+	아이디어 : bfs와 dfs 두 방법 모두 이용해 풀어보았습니다.
+		bfs : (row, col) 인접한 곳 방문안되었으면 방문처리, q에 넣음 모든 q가 빌때까지 반복
+		dfs : (row, col) 과 인접한 곳 방문안되었으면 그곳으로 다시 방문해서 dfs 반복
+*/
+
 public class Main {
 	
+	// 각 (row, col) 값을 담기 위한 클래스
 	static class Node {
 		int row;
 		int col;
@@ -19,13 +31,26 @@ public class Main {
 	static int w, h;
 	static int[][] island;
 	static boolean[][] isVisited;
+	// 8방향
 	static int[][] dxdy = {{-1, -1}, {-1, 0}, {-1, 1},
 			{0, -1}, {0, 1},
 			{1, -1}, {1, 0}, {1, 1}};
 	
-//	static void dfs() {
-//		
-//	}
+	static void dfs(int row, int col) {
+		
+		isVisited[row][col] = true;
+		
+		for (int i=0; i<8; i++) {
+			int dx = row + dxdy[i][0];
+			int dy = col + dxdy[i][1];
+			
+			if (dx >=0 && dx<h && dy>=0 && dy<w) {
+				if(!isVisited[dx][dy] && island[dx][dy] == 1) {
+					dfs(dx,  dy);
+				}
+			}
+		}
+	}
 	
 	static void bfs(int row, int col) {
 		
@@ -45,6 +70,7 @@ public class Main {
 				
 				if (dx >=0 && dx<h && dy>=0 && dy<w) {
 					if(!isVisited[dx][dy] && island[dx][dy] == 1) {
+						// bfs는 항상 q에 넣을 때 방문처리!
 						isVisited[dx][dy] = true;
 						q.offer(new Node(dx, dy));
 					}
@@ -88,9 +114,21 @@ public class Main {
 				}
 			}
 			
+			ans = 0;
+			isVisited = new boolean[h][w];
+			
+			// dfs
+			for (int i=0; i<h; i++) {
+				for (int j=0; j<w; j++) {
+					if (!isVisited[i][j] && island[i][j] == 1) {
+						dfs(i, j);
+						ans+=1;
+					}
+				}
+			}
+			
 			System.out.println(ans);
 			
-//			dfs();
 		}
 		
 	}
