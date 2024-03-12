6bb0d088bd321f9def9b30aeb8f7d9273bcec8f6
코코형아 <92447290+Nbowow@users.noreply.github.com>
2024-03-12 09:24:38 +0900

    [Gold V] Title: 괄호의 값, Time: 88 ms, Memory: 11916 KB -BaekjoonHub

---
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.java
@@ -0,0 +1,57 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		char[] bracket = br.readLine().toCharArray();
+		Stack<Character> st = new Stack<>() ;
+		
+		int ans  = 0;
+		int value = 1;
+		for (int i=0; i<bracket.length; i++) {
+			if (bracket[i] == '(') {
+				st.push(bracket[i]);
+				value *= 2;
+			}
+			
+			else if (bracket[i] == '[') {
+				st.push(bracket[i]);
+				value *= 3;
+			}
+			
+			else if (bracket[i] == ')') {
+				if (st.isEmpty() || st.peek() == '[') {
+					System.out.println(0);
+					System.exit(0);
+				}
+				
+				else if (bracket[i-1] == '(') {
+					ans += value;
+				}
+				value /= 2;
+				st.pop();
+			}
+			
+			else if (bracket[i] == ']') {
+				if (st.isEmpty() || st.peek() == '(') {
+					System.out.println(0);
+					System.exit(0);
+				}
+				
+				else if (bracket[i-1] == '[') {
+					ans += value;
+				}
+				value /= 3;
+				st.pop();
+			}
+		}
+		
+		if (st.isEmpty()) System.out.println(ans);
+		else System.out.println(0);
+	}
+
+}
