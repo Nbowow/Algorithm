27d25b7310f59e4421f4e34648180c2e45dc9c0f
코코형아 <92447290+Nbowow@users.noreply.github.com>
2024-03-28 10:55:08 +0900

    [Gold IV] Title: 플로이드, Time: 384 ms, Memory: 46840 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static class Node {
+		int arrive;
+		int cost;
+		public Node(int arrive, int cost) {
+			this.arrive = arrive;
+			this.cost = cost;
+		}
+	}
+
+	static int n, m;
+	static List<List<Node>> node = new ArrayList<>();
+	static int[][] dp;
+	static final int MAX_COST = 100000;
+	static final int MAX_BUS = 100;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		m = Integer.parseInt(br.readLine());
+		
+		dp = new int[n+1][n+1];
+//		for (int i=1; i<n+1; i++) Arrays.fill(dp[i], Integer.MAX_VALUE); <- overflow 날 수 있음
+		for (int i=1; i<n+1; i++) Arrays.fill(dp[i], MAX_BUS * MAX_COST);
+		
+		for (int i=1; i<n+1; i++) dp[i][i] = 0;
+		
+		for (int i=0; i<n+1; i++) node.add(new ArrayList<>());
+		for (int i=0; i<m; i++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			
+			node.get(a).add(new Node(b, c));
+			dp[a][b] = Math.min(dp[a][b], c);
+		}
+		
+		for (int k=1; k<n+1; k++) { // 경로
+			for (int i=1; i<n+1; i++) { // 출발지
+				for (int j=1; j<n+1; j++) { // 도착지
+					dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k][j]);
+				}
+				
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i=1; i<n+1; i++) {
+			for (int j=1; j<n+1; j++) {
+				if (dp[i][j] == MAX_BUS * MAX_COST) dp[i][j] = 0;
+				sb.append(dp[i][j] + " ");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+		
+//		for (int i=1; i<n+1; i++) {
+//			for (int j=1; j<n+1; j++) {
+//				System.out.print(dp[i][j] + " ");
+//			}
+//			System.out.println();
+//		}
+	}
+
+}
