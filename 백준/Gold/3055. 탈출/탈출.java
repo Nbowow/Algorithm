a3489da85005683a0c7a9c43e2c212039c229732
코코형아 <92447290+Nbowow@users.noreply.github.com>
2024-03-28 14:49:15 +0900

    [Gold IV] Title: 탈출, Time: 96 ms, Memory: 13028 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.java
+++ 백준/Gold/3055. 탈출/탈출.java
@@ -1,122 +1,114 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int R, C, ans;
-	static boolean isArrive;
-	static boolean[][] visitedWater;
-	static boolean[][] visitedGosum;
-	static Character[][] map;
+	static int R, C;
+	static char[][] map;
+	static Queue<int[]> wq = new ArrayDeque<>();
+	static Queue<int[]> sq = new ArrayDeque<>();
 	static int[][] dxdy = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-	static Queue<int[]> qGosum = new ArrayDeque<>();
-	static Queue<int[]> qWater = new ArrayDeque<>();
+	static boolean[][] wVisited;
+	static boolean[][] sVisited;
 	
-	
-	static void checkPath(int del, int row, int col) {
-		for (int i=0; i<4; i++) {
-			int dx = row + dxdy[i][0];
-			int dy = col + dxdy[i][1];
+	static void bfs() {
+		
+		while (!wq.isEmpty() || !sq.isEmpty()) {
+			// 물 이동
+			int wSize = wq.size();
 			
-			if (dx >= 0 && dx < R && dy >= 0 && dy < C) {
-				// 물
-				if (del == 1 && (map[dx][dy] == '.' || map[dx][dy] == 'S')) {
-					map[dx][dy] = '*';
+			for (int w=0; w<wSize; w++) {
+				int[] wtemp = wq.poll();
+				int wx = wtemp[0];
+				int wy = wtemp[1];
+				
+				for (int i=0; i<4; i++) {
+					int wdx = wx + dxdy[i][0];
+					int wdy = wy + dxdy[i][1];
+					
+					if (!isIn(wdx, wdy) || wVisited[wdx][wdy]) continue;
+					wVisited[wdx][wdy] = true;
+					
+					if (map[wdx][wdy] == 'D' || map[wdx][wdy] == 'X') continue;
+					
+					map[wdx][wdy] = '*';
+					wq.offer(new int[] {wdx, wdy});
+				}
+			}
+			
+			// 고슴도치 이동
+			int sSize = sq.size();
+			
+			for (int s=0; s<sSize; s++) {
+				int[] stemp = sq.poll();
+				int sx = stemp[0];
+				int sy = stemp[1];
+				int time = stemp[2];
+				
+				for (int i=0; i<4; i++) {
+					int sdx = sx + dxdy[i][0];
+					int sdy = sy + dxdy[i][1];
+					
+					if (!isIn(sdx, sdy) || sVisited[sdx][sdy]) continue;
+					sVisited[sdx][sdy] = true;
+					
+					// 비버굴 도착
+					if (map[sdx][sdy] == 'D') {
+						System.out.println(time + 1); // 시간 출력
+						System.exit(0);
+					}
+					
+					// 물
+					if (map[sdx][sdy] == '*' || map[sdx][sdy] == 'X') continue;
+					
+					map[sdx][sdy] = 'S';
+					sq.offer(new int[] {sdx, sdy, time + 1});
 				}
 				
-				// 고슴도치
-				else if (del == 2) {
-					// 집 도착
-					if (map[dx][dy] == 'D') {
-						isArrive = true;
-						return;
-					}
-					else if (map[dx][dy] == '.') {
-						map[dx][dy] = 'S';	
-					}
-				}
+				
 			}
-		}
-		
-//		System.out.println("del : " + del);
-//		printMap();
-	}
-	
 
-	static void lookingHome(int count) {
-		
-		boolean isfail = true;
-		
-		// 고슴도치 먼저 이동
-		for (int i=0; i<R; i++) {
-			for (int j=0; j<C; j++) {				
-				if (visitedGosum[i][j] == false && map[i][j] == 'S') {
-					visitedGosum[i][j] = true;
-					isfail = false;
-					// 모든 고슴도치 좌표 큐에 담음
-					qGosum.offer(new int[] {i, j});
-//					checkPath(2, i, j);
-				}
-			}
 		}
-		
-		// 고슴도치 처리
-		while(!qGosum.isEmpty()) {
-			int[] temp = qGosum.poll();
-			int r = temp[0];
-			int c = temp[1];
-			checkPath(2, r, c);
-		}
-		
-		// 물로 덮음
-		for (int i=0; i<R; i++) {
-			for (int j=0; j<C; j++) {
-				if (visitedWater[i][j] == false && map[i][j] == '*') {
-					visitedWater[i][j] = true;
-					// 모든 물 좌표 큐에 담음
-					qWater.offer(new int[] {i, j});
-				}
-			}
-		}
-		
-		// 물 처리
-		while(!qWater.isEmpty()) {
-			int[] temp = qWater.poll();
-			int r = temp[0];
-			int c = temp[1];
-			checkPath(1, r, c);
-		}
-		
-		if (isArrive) {
-			ans = count;
-			return;
-		} else if(isfail) return;
-		else lookingHome(count + 1);
+
 	}
 	
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		R = Integer.parseInt(st.nextToken());
 		C = Integer.parseInt(st.nextToken());
-		map = new Character[R][C];
-		visitedWater = new boolean[R][C];
-		visitedGosum = new boolean[R][C];
 		
+		map = new char[R][C];
+		wVisited = new boolean[R][C];
+		sVisited = new boolean[R][C];
 		for (int i=0; i<R; i++) {
 			String str = br.readLine();
 			for (int j=0; j<C; j++) {
 				map[i][j] = str.charAt(j);
+				if (map[i][j] == '*') {
+					wq.offer(new int[] {i, j});
+					wVisited[i][j] = true;
+				}
+				
+				if (map[i][j] == 'S') {
+					sq.offer(new int[] {i, j, 0});
+					sVisited[i][j] = true;
+				}
 			}
 		}
-		
-		lookingHome(1);
-		
-		if (isArrive) System.out.println(ans);
-		else System.out.println("KAKTUS");
+	
+		bfs();
+		System.out.println("KAKTUS");
+	}
+	
+	static boolean isIn(int x, int y) {
+		if (x>=0 && x<R && y>=0 && y<C) return true;
+		return false;
 	}
 	
 	static void printMap() {
@@ -126,8 +118,7 @@ public class Main {
 			}
 			System.out.println();
 		}
-		
-		System.out.println();
 	}
+	
 
 }
