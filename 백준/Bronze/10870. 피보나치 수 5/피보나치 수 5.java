c0fd52f5469954b6b1230c5433006677fd3787a1
코코형아 <92447290+Nbowow@users.noreply.github.com>
2024-04-02 14:49:26 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 80 ms, Memory: 11652 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -1,21 +1,26 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Main {
-	
-	static int fibo(int N) {
-		if (N == 0) return 0;
-		else if (N == 1) return 1;
-		
-		return fibo(N-1) + fibo(N-2);
-	}
-	
-	public static void main(String[] args) throws IOException{
+
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 		int n = Integer.parseInt(br.readLine());
 		
-		System.out.println(fibo(n));
+		if (n == 0) System.out.println(0);
+		else {
+			BigInteger[] dp = new BigInteger[n+1];
+			dp[0] = new BigInteger("0");
+			dp[1] = new BigInteger("1");
+			for (int i=2; i<n+1; i++) {
+				dp[i] = dp[i-1].add(dp[i-2]);
+			}
+			
+			System.out.println(dp[n]);
+		}
 		
 	}
+
 }
