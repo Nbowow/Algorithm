ae908482e2cf2c8d6946850668901c377b80edbc
코코형아 <92447290+Nbowow@users.noreply.github.com>
2024-04-03 17:40:20 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,293 ms, Memory: 108,880 KB -BaekjoonHub

---
+++ SWEA/D4/5643. ［Professional］ 키 순서/README.md
@@ -0,0 +1,15 @@
+# [D4] [Professional] 키 순서 - 5643 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWXQsLWKd5cDFAUo) 
+
+### 성능 요약
+
+메모리: 108,880 KB, 시간: 1,293 ms, 코드길이: 1,615 Bytes
+
+### 제출 일자
+
+2024-04-03 17:40
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	static int[] nums;
+	static List<List<Integer>> nodes;
+	
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int tc=1; tc<=T; tc++) {
+			int N = Integer.parseInt(br.readLine());
+			int M = Integer.parseInt(br.readLine());
+			
+			nums = new int[N+1];
+			nodes = new ArrayList<>();
+			for (int i=0; i<N+1; i++) {
+				nodes.add(new ArrayList<>());
+			}
+			
+			// 비교횟수
+			for (int i=0; i<M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				
+				// a에서 b로 가는 경로
+				nodes.get(a).add(b);
+			}
+			
+			for (int i=1; i<nodes.size(); i++) {
+				Queue<Integer> q = new ArrayDeque<>();
+				boolean[] isVisited = new boolean[N+1];
+				isVisited[i] = true;
+				q.offer(i);
+				
+				while (!q.isEmpty()) {
+					int cur = q.poll();
+					
+					for (int j=0; j<nodes.get(cur).size(); j++) {
+						int next = nodes.get(cur).get(j);
+						if (!isVisited[next]) {
+							isVisited[next] = true;
+							nums[next] += 1;
+							nums[i]++;
+							q.offer(next);
+						}
+					}
+				}
+				
+			}
+			
+			int ans = 0;
+			for (int i=1; i<N+1; i++) {
+				if (nums[i] == N-1) ans++;
+			}
+			System.out.println("#" + tc + " " + ans);
+		}
+		
+		
+	}
+
+}
