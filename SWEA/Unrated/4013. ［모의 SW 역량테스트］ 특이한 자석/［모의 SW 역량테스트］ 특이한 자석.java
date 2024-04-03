3a72846c9d2c7f754e0c742edcf07065bb92eb08
코코형아 <92447290+Nbowow@users.noreply.github.com>
2024-04-03 15:01:22 +0900

    [Unrated] Title: [모의 SW 역량테스트] 특이한 자석, Time: 165 ms, Memory: 23,768 KB -BaekjoonHub

---
+++ SWEA/Unrated/4013. ［모의 SW 역량테스트］ 특이한 자석/［모의 SW 역량테스트］ 특이한 자석.java
@@ -0,0 +1,118 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	
+	static int[][] wheels = new int[4][8];
+    static int K, ans;
+
+    static void rotateWheels(int num, int dir) {
+        if (dir == 1) { // 시계 방향 회전
+            int temp = wheels[num][7];
+            for (int i = 7; i > 0; i--) { // 뒤에서부터 하나씩 당겨옴
+                wheels[num][i] = wheels[num][i - 1];
+            }
+            wheels[num][0] = temp;
+        } else if (dir == -1) { // 반시계 방향 회전
+            int temp = wheels[num][0];
+            for (int i = 0; i < 7; i++) {
+                wheels[num][i] = wheels[num][i + 1];
+            }
+            wheels[num][7] = temp;
+        }
+
+    }
+
+    static void rotate(int num, int dir) {
+        int f1 = 0;
+        int f2 = 0;
+        int f3 = 0;
+        int f4 = 0;
+        int reverseDir = dir * (-1);
+
+        if (num == 1) {
+            f1 = dir;
+            if (wheels[0][2] != wheels[1][6]) { // 2번도 돌려야함
+                f2 = reverseDir;
+                if (wheels[1][2] != wheels[2][6]) { // 3번도 돌려야함
+                    f3 = dir;
+                    if (wheels[2][2] != wheels[3][6]) f4 = reverseDir; // 4번도 돌려야함
+                }
+            }
+        }
+
+        if (num == 2) {
+            f2 = dir;
+            if (wheels[1][6] != wheels[0][2]) f1 = reverseDir;
+            if (wheels[1][2] != wheels[2][6]) {
+                f3 = reverseDir;
+                if (wheels[2][2] != wheels[3][6]) f4 = dir;
+            }
+        }
+
+        if (num == 3) {
+            f3 = dir;
+            if (wheels[2][2] != wheels[3][6]) f4 = reverseDir; // 4번
+            if (wheels[2][6] != wheels[1][2]) {
+                f2 = reverseDir;
+                if (wheels[1][6] != wheels[0][2]) f1 = dir;
+            }
+        }
+
+        if (num == 4) {
+            f4 = dir;
+            if (wheels[3][6] != wheels[2][2]) {
+                f3 = reverseDir;
+                if (wheels[2][6] != wheels[1][2]) {
+                    f2 = dir;
+                    if (wheels[1][6] != wheels[0][2]) f1 = reverseDir;
+                }
+            }
+
+        }
+        if (f1 != 0) rotateWheels(0, f1);
+        if (f2 != 0) rotateWheels(1, f2);
+        if (f3 != 0) rotateWheels(2, f3);
+        if (f4 != 0) rotateWheels(3, f4);
+    }
+
+	public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        for (int tc=1; tc<=T; tc++) {
+        	ans = 0;
+        	K = Integer.parseInt(br.readLine());
+        	
+        	for (int i = 0; i < 4; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < 8; j++) {
+                    wheels[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            
+            for (int i = 0; i < K; i++) {
+                st = new StringTokenizer(br.readLine());
+                int N = Integer.parseInt(st.nextToken()); // 톱니 번호
+                int D = Integer.parseInt(st.nextToken()); // 방향 (1 : 시계, -1 : 반시계)
+
+                rotate(N, D);
+            }
+
+            if (wheels[0][0] == 1) ans += 1;
+            if (wheels[1][0] == 1) ans += 2;
+            if (wheels[2][0] == 1) ans += 4;
+            if (wheels[3][0] == 1) ans += 8;
+
+
+            System.out.println("#" + tc + " " + ans);
+            
+        }
+
+	}
+
+}
