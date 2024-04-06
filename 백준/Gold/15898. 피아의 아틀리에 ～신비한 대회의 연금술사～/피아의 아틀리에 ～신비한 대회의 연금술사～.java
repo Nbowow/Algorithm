930881967c6ce4a1c34c0e6aecf33a2abffb02e8
코코형아 <92447290+Nbowow@users.noreply.github.com>
2024-04-06 17:49:47 +0900

    [Gold I] Title: 피아의 아틀리에 ~신비한 대회의 연금술사~, Time: 2880 ms, Memory: 301032 KB -BaekjoonHub

---
+++ 백준/Gold/15898. 피아의 아틀리에 ～신비한 대회의 연금술사～/피아의 아틀리에 ～신비한 대회의 연금술사～.java
@@ -0,0 +1,192 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static int n;
+    static int[][][][] effect;
+    static char[][][][] color;
+    static List<Integer> itemPer = new ArrayList<>();
+    static boolean[] isVisited;
+    static List<Integer> rotatePer = new ArrayList<>();
+    static List<Integer> putPer = new ArrayList<>();
+    static Map<Integer, int[]> map = new HashMap<>();
+    static int ans;
+
+    // 뽑은 아이템, 회전 수, 넣는 위치를 뽑은 뒤 폭탄 품질 구하기
+    static void calculate() {
+
+        // 초기 배열
+        int[][] effectGama = new int[5][5];
+        char[][] colorGama = new char[5][5];
+
+        // 각 배열에 해당하는 동작 수행
+        for (int i = 0; i < 3; i++) {
+            int itemIdx = itemPer.get(i);
+            int rotateIdx = rotatePer.get(i);
+            int putIdx = putPer.get(i);
+
+            int[][] cloneEffect = copyEffect(itemIdx, rotateIdx);
+            char[][] cloneColor = copyColor(itemIdx, rotateIdx);
+
+            int[] put = map.get(putIdx);
+            int x = put[0];
+            int y = put[1];
+
+            for (int a = 0; a < 4; a++) {
+                for (int b = 0; b < 4; b++) {
+                    // 품질
+                    effectGama[a+x][b+y] += cloneEffect[a][b];
+                    if (effectGama[a+x][b+y] < 0) effectGama[a+x][b+y] = 0;
+                    else if (effectGama[a+x][b+y] > 9) effectGama[a+x][b+y] = 9;
+
+                    // 색상 (흰색이 아닐때만 색칠)
+                    if (cloneColor[a][b] != 'W') colorGama[a+x][b+y] = cloneColor[a][b];
+                }
+
+            }
+
+        }
+
+        // 완성된 가마에 대해 품질 구하기
+        int count = 0;
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (colorGama[i][j] == 'R') count += (7 * effectGama[i][j]);
+                else if (colorGama[i][j] == 'B') count += (5 * effectGama[i][j]);
+                else if (colorGama[i][j] == 'G') count += (3 * effectGama[i][j]);
+                else if (colorGama[i][j] == 'Y') count += (2 * effectGama[i][j]);
+            }
+        }
+        ans = Math.max(ans, count);
+    }
+
+    static void putPermutation(int depth) {
+
+        if (depth == 3) { // 순열 뽑음
+            calculate();
+            return;
+        }
+
+        // 넣는 위치 4곳 중 하나 뽑기
+        for (int i = 0; i < 4; i++) {
+            putPer.add(i);
+            putPermutation(depth + 1);
+            putPer.remove(putPer.size() - 1);
+        }
+    }
+
+    static void rotatePermutation(int depth) {
+
+        if (depth == 3) { // 순열 뽑음
+            // 가마에 넣는 순열
+            putPermutation(0);
+            return;
+        }
+
+        // 4방향 중 하나 뽑기
+        for (int i = 0; i < 4; i++) {
+            rotatePer.add(i);
+            rotatePermutation(depth + 1);
+            rotatePer.remove(rotatePer.size() - 1);
+        }
+    }
+
+    static void itemPermutation(int depth) {
+        if (depth == 3) { // 재료 순열 뽑음 (단, 중복 X)
+            // 회전 순열
+            rotatePermutation(0);
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!isVisited[i]) {
+                isVisited[i] = true;
+                itemPer.add(i);
+                itemPermutation(depth + 1);
+                isVisited[i] = false;
+                itemPer.remove(itemPer.size() - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+
+        // (r, c, n) : row, col, n번째, 회전
+        effect = new int[4][4][n][4];
+        color = new char[4][4][n][4];
+        for (int c = 0; c < n; c++) {
+            // 효능
+            for (int i = 0; i < 4; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < 4; j++) {
+                    effect[i][j][c][0] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            // 원소
+            for (int i = 0; i < 4; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < 4; j++) {
+                    color[i][j][c][0] = st.nextToken().charAt(0);
+                }
+            }
+        }
+
+        // 배열 돌리기
+        for (int c = 0; c < n; c++) { // 모든 재료에 대해
+            for (int rotate = 1; rotate < 4; rotate++) { // 90도, 180도, 270도 회전
+                // 효능
+                for (int i = 0; i < 4; i++) {
+                    for (int j = 0; j < 4; j++) {
+                        effect[i][j][c][rotate] = effect[4-(1+j)][i][c][rotate-1];
+                    }
+                }
+
+                // 원소
+                for (int i = 0; i < 4; i++) {
+                    for (int j = 0; j < 4; j++) {
+                        color[i][j][c][rotate] = color[4-(1+j)][i][c][rotate-1];
+                    }
+                }
+            }
+        }
+
+        // 재료 놓는 위치
+        map.put(0, new int[]{0, 0});
+        map.put(1, new int[]{0, 1});
+        map.put(2, new int[]{1, 0});
+        map.put(3, new int[]{1, 1});
+
+        isVisited = new boolean[n];
+        // 재료 순열
+        itemPermutation(0);
+
+        System.out.println(ans);
+
+    }
+
+    static int[][] copyEffect(int idx, int dir) {
+        int[][] temp = new int[4][4];
+        for (int i=0; i<4; i++) {
+            for (int j = 0; j < 4; j++) {
+                temp[i][j] = effect[i][j][idx][dir];
+            }
+        }
+        return temp;
+    }
+
+    static char[][] copyColor(int idx, int dir) {
+        char[][] temp = new char[4][4];
+        for (int i=0; i<4; i++) {
+            for (int j = 0; j < 4; j++) {
+                temp[i][j] = color[i][j][idx][dir];
+            }
+        }
+        return temp;
+    }
+}
