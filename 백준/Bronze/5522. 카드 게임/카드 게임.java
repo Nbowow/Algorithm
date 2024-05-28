22e9c4ff2840666eee01ef2a53df3b8f4631c6c3
코코형아 <92447290+Nbowow@users.noreply.github.com>
2024-05-28 22:59:31 +0900

    [Bronze V] Title: 카드 게임, Time: 80 ms, Memory: 11496 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            sum += Integer.parseInt(br.readLine());
+        }
+
+        System.out.println(sum);
+
+    }
+}
