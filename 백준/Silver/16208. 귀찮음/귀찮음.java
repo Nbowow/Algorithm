29471681c0ae128d9f226cccb7ce6e46105b35e2
코코형아 <92447290+Nbowow@users.noreply.github.com>
2024-11-01 16:38:42 +0900

    [Silver V] Title: 귀찮음, Time: 68 ms, Memory: 11540 KB, Score: 4 point -BaekjoonHub

---
+++ 백준/Silver/16208. 귀찮음/귀찮음.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        List<Integer> nums = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            nums.add(Integer.parseInt(st.nextToken()));
+        }
+
+        long ans = 0;
+        while (nums.size() > 1) {
+            Collections.sort(nums);
+
+            int size = nums.size();
+            List<Integer> temp = new ArrayList<>();
+            for (int i = 0; i < size / 2; i++) {
+                ans += nums.get(i) * nums.get(size - i - 1);
+                temp.add(nums.get(i)+nums.get(size - i - 1));
+            }
+
+            // 홀수
+            if (size % 2 == 1) {
+                temp.add(nums.get(size / 2));
+            }
+
+            nums = new ArrayList<>();
+            for (int i = 0; i < temp.size(); i++) {
+                nums.add(temp.get(i));
+            }
+
+        }
+
+        System.out.println(ans);
+
+    }
+
+}
